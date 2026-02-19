ed43d0cb972073b3f929849ef1c6367d9c31b2f1
Klaudia Kyc <klaudia.kyc@gmail.com>
2026-02-19 12:42:05 +0100

    database

--- app.py
+++ app.py
@@ -1,55 +1,74 @@
 from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask("what a knit")
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///knit.db'
+db = SQLAlchemy(app)
 
-patterns=[
-    {
-        'name': 'Monday sweater',
-        'type': 'sweater',
-        'subtype': 'raglan',
-        'tool': 'needles',
-        'needle_size': '4mm',
-        'yarn_bought': True,
-        'difficulty': 1,
-        'status': 'in progress',
-        'completion': 60,
-        'rating': 5,
-        'language': 'polish',
-        'designer' : 'Petite knit',
-        'notes': ''
-    },
-    {
-        'name': 'Boucle hat',
-        'type': 'hat',
-        'subtype': 'double folded',
-        'tool': 'needles',
-        'needle_size': '4mm',
-        'yarn_bought': True,
-        'difficulty': 3,
-        'status': 'in progress',
-        'completion': 90,
-        'rating': 3,
-        'language': None,
-        'designer': 'me',
-        'notes': ''
-    },
-    {
-        'name': 'Wesley socks',
-        'type': 'socks',
-        'subtype': 'heel flap',
-        'tool': 'needles',
-        'needle_size': '2.5mm',
-        'yarn_bought': False,
-        'difficulty': None,
-        'status': 'not started',
-        'completion': 0,
-        'rating': None,
-        'language': 'polish',
-        'designer': 'Knitted moments',
-        'notes': ''
-    },
-]
+class Pattern(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False) #cannot be empty
+    type = db.Column(db.String(50))
+    subtype = db.Column(db.String(50))
+    tool = db.Column(db.String(20))
+    needle_size = db.Column(db.String(20))
+    designer = db.Column(db.String(50))
+    language = db.Column(db.String(50))
+    difficulty = db.Column(db.Integer)
+    yarn_bought = db.Column(db.String(3))
+    status = db.Column(db.String(50))
+    completion = db.Column(db.Integer)
+    rating = db.Column(db.Integer)
+    notes = db.Column(db.String(500))
+
+# patterns=[
+#     {
+#         'name': 'Monday sweater',
+#         'type': 'sweater',
+#         'subtype': 'raglan',
+#         'tool': 'needles',
+#         'needle_size': '4mm',
+#         'yarn_bought': True,
+#         'difficulty': 1,
+#         'status': 'in progress',
+#         'completion': 60,
+#         'rating': 5,
+#         'language': 'polish',
+#         'designer' : 'Petite knit',
+#         'notes': ''
+#     },
+#     {
+#         'name': 'Boucle hat',
+#         'type': 'hat',
+#         'subtype': 'double folded',
+#         'tool': 'needles',
+#         'needle_size': '4mm',
+#         'yarn_bought': True,
+#         'difficulty': 3,
+#         'status': 'in progress',
+#         'completion': 90,
+#         'rating': 3,
+#         'language': None,
+#         'designer': 'me',
+#         'notes': ''
+#     },
+#     {
+#         'name': 'Wesley socks',
+#         'type': 'socks',
+#         'subtype': 'heel flap',
+#         'tool': 'needles',
+#         'needle_size': '2.5mm',
+#         'yarn_bought': False,
+#         'difficulty': None,
+#         'status': 'not started',
+#         'completion': 0,
+#         'rating': None,
+#         'language': 'polish',
+#         'designer': 'Knitted moments',
+#         'notes': ''
+#     },
+# ]
 
 @app.route('/')
 def main_page():
@@ -68,34 +87,35 @@ def main_page():
             {'color': 'Zielona', 'weight': 200}
         ],
         message='Witaj w świecie Jinja2!',
-        patterns=patterns,
+        patterns=Pattern.query.all(), #give me all records from table - list[Objects]
     )
 
 @app.route('/pattern/<int:pattern_id>')
 def pattern_detail(pattern_id):
-    pattern = patterns[pattern_id]
+    pattern = db.get_or_404(Pattern, pattern_id)
     return render_template('pattern_detail.html', pattern=pattern)
 
 @app.route('/add', methods=['GET', 'POST'])
 def add_pattern():
     if request.method == 'POST':
-        new_project = {
-            'name': request.form['name'],
-            'type': request.form['type'],
-            'subtype': request.form['subtype'],
-            'tool': 'needles',
-            'needle_size': '2.5mm',
-            'yarn_bought': False,
-            'difficulty': None,
-            'status': 'not started',
-            'completion': 0,
-            'rating': None,
-            'language': 'polish',
-            'designer': 'Knitted moments',
-            'notes': ''
-        }
-        patterns.append(new_project)
-        return redirect(f'/pattern/{len(patterns) - 1}')
+        new_pattern = Pattern(
+            name=request.form['name'],
+            type=request.form['type'],
+            subtype=request.form['subtype'],
+            tool='needles',
+            needle_size='2.5mm',
+            yarn_bought=False,
+            difficulty=None,
+            status='not started',
+            completion=0,
+            rating=None,
+            language='polish',
+            designer='Knitted moments',
+            notes=request.form['notes']
+        )
+        db.session.add(new_pattern)
+        db.session.commit()
+        return redirect(f'/pattern/{new_pattern.id}')
     return render_template('add_pattern.html')
 
 if __name__ == '__main__':
